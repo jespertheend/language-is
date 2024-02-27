d574d02f10e5efc49614286499e514a5b46b51c1
Jesper van den Ende <jespertheend@gmail.com>
2024-02-27 19:01:04 +0100

    use language as title

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
 Deno.serve(req => {
+	const lang = req.headers.get("Accept-Language");
 	return new Response(`
 <!DOCTYPE html>
 <html>
+	<head>
+		<title>${lang}</title>
+		<meta property="og:title" content="${lang}">
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://letter.open-web-advocacy.org/">
+		<meta name="twitter:card" content="summary_large_image">
+		<meta property="og:image" content="https://letter.open-web-advocacy.org/images/og-image.png">
+		<meta name="twitter:image" content="https://letter.open-web-advocacy.org/images/og-image.png">
+		<meta property="og:image:alt" content="Page image for Open Letter to Tim Cook, Sabotaging Web Apps Is Indefensible">
+		<meta name="twitter:image:alt" content="Page image for Open Letter to Tim Cook, Sabotaging Web Apps Is Indefensible">
+	</head>
 	<body>
-		<p>hello</p>
+		<p>${lang}</p>
 	</body>
 </html
 	`, {
